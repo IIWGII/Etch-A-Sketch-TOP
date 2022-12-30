f65f9c7aea46c5ae14e9efe54b4d5ed6945b68e4
IIWGII <Himdi51@gmail.com>
2022-12-30 19:13:14 +0100

    first commit: created html/css and JS files and html boilerplate

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.grid-container');
+const gridDiv = document.createElement('div');
+
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
+container.appendChild(gridDiv);
