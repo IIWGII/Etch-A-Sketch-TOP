c57452bd23aca6ede8f955ca4772e0060f3a7569
IIWGII <Himdi51@gmail.com>
2022-12-30 19:58:14 +0100

    created the grid and hover effect with JavaScript

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,17 @@
 const container = document.querySelector('.grid-container');
-const gridDiv = document.createElement('div');
 
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
-container.appendChild(gridDiv);
+
+for (let i=0; i < 256; i++ ) {
+    const createDiv = document.createElement('div');
+    container.appendChild(createDiv);
+}
+
+const divs = container.querySelectorAll('div');
+divs.forEach((div) => {
+    div.addEventListener('mouseover', (e)=>{
+        e.target.style.backgroundColor = "black"
+    });
+    div.addEventListener('mouseout', (e) => {
+        e.target.style.backgroundColor = "white"
+    })
+})
